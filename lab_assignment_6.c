7ef1b1767117145f73faba6c232abd715b745c22
DinoDerk <Derekflorer@knights.ucf.edu>
2023-02-23 23:57:52 -0500

    Recursive function with needed tet file

---
+++ lab_assignment_6.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int search(int numbers[], int low, int high, int value) 
+{
+	// If high is less than 0, the value does not occur in the array
+	if(high < 0)
+		return -1;
+
+	// If high is equal to value, return index high
+	if (numbers[high] == value)
+		return (high);
+
+	// Updates high to decrease by one for the next path through
+	high--;
+
+	// Call the funtion in a recursive manner
+	search(numbers, 0, high, value);
+}
+
+void printArray(int numbers[], int sz)
+{
+	int i;
+	printf("Number array : ");
+	for (i = 0;i<sz;++i)
+	{
+		printf("%d ",numbers[i]);
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	int i, numInputs;
+	char* str;
+	float average;
+	int value;
+	int index;
+	int* numArray = NULL;
+	int countOfNums;
+	FILE* inFile = fopen("input.txt","r");
+
+	fscanf(inFile, " %d\n", &numInputs);
+	
+	while (numInputs-- > 0)
+	{
+		fscanf(inFile, " %d\n", &countOfNums);
+		numArray = (int *) malloc(countOfNums * sizeof(int));
+		average = 0;
+		for (i = 0; i < countOfNums; i++)
+		{
+			fscanf(inFile," %d", &value);
+			numArray[i] = value;
+			average += numArray[i];
+		}
+
+		printArray(numArray, countOfNums);
+		value = average / countOfNums;
+		index = search(numArray, 0, countOfNums - 1, value);
+		if (index >= 0)
+		{
+			printf("Item %d exists in the number array at index %d!\n",value, index);
+		}
+		else
+		{
+			printf("Item %d does not exist in the number array!\n", value);
+		}
+
+		free(numArray);
+	}
+
+	fclose(inFile);
+}
